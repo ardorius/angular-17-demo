a444b4fd59bb4a12c002160a9ef41cc8814a6eea
Peter Petruš <petrus.peterhe@gmail.com>
2023-11-19 17:30:27 +0100

    preparation for angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgClass, NgIf, AsyncPipe } from '@angular/common';
+import { NgClass, AsyncPipe } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
@@ -9,7 +9,7 @@ import { FooterComponent } from './footer/footer.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [NgClass, HeaderComponent, RouterOutlet, NgIf, FooterComponent, AsyncPipe]
+  imports: [NgClass, HeaderComponent, RouterOutlet, FooterComponent, AsyncPipe]
 })
 export class AppComponent {
   title = 'angular-17-demo';
